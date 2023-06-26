6a3fb79967a14c618d1dddd78a77c1a334041e1d
Yahshemi <yahshemi@gmail.com>
2023-06-26 00:16:38 -0700

    adds functionality

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+// Calculate Decimal
+function timeToDecimal(){
+hours + minutes * (1/60) + seconds * (1/ 3600)
+}
 
+// Handle Click Event
+function handleClickEvent(){
+    let hours = $('#hours').val();
+    let minutes = $('#minutes').val();
+    let seconds = $('#seconds').val();
 
+// Result
+let result = timeToDecimal(hours, minutes, seconds);
+    console.log('you clicked me');
+    console.log(hours, minutes, seconds);
+    console.log(result);
 
-// Calculate function
-function calculate(){
-    console.log('you clicked me')
 };
